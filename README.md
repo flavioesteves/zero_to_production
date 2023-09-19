83ab6faf5032b39cc20d4e4a4be63166238aa886
Flavio Esteves <flavioafesteves@gmail.com>
2023-09-19 22:59:59 +0100

    3.5.1.2

--- README.md
+++ README.md
@@ -1,30 +1,33 @@
 # Interpretation of the book "Zero To Production in Rust"
 
 
-## Cargo
+# Cargo
     1. cargo watch -x check
         *  Will run cargo check after every code change
     2. cargo watch -x check -x test -x run
         * Runs cargo check.
         * It it succeds, launches carto test.
         * It tests pass launches the app with cargo run.
-
-## CI
+    3. cargo check
+        * check if is possibel to compile
+    4. carrgo expand
+# CI
     1. Code Coverage
         * cargo tarpaulin --ignore-tests
-## Linting - clippy
+# Linting - clippy
     1. rustup component add clippy
     2. cargo clippy
     3. cargo clippy -- -D warnings
         * CI pipeline would fail if the linter check clippy emits any warnings
 
-## Formatting
+# Formatting
     1. rustup component add rustfmt
     2. cargo fmt
         * Format the whole project
     3. cargo fmt -- --check 
         * Add formatting step to the pipeline
 
-## Security Vulnerabilities
+# Security Vulnerabilities
     1. cargo install cargo-audit
     2. cargo audit
+
---
+++ tests/health_check.rs
@@ -0,0 +1,36 @@
+// `tokio::test` is the testing equivalent of `tokio::main`.
+// It also spares you from having to specify the `#[test]` attribute.
+//
+// You can inspect what code gets generated using
+// `cargo expand --test health_check` (<- name of the test file)
+
+use std::net::TcpListener;
+
+#[tokio::test]
+async fn health_check_works() {
+    // Arrange
+    let address = spawn_app();
+    // We need to bring `reqwest`
+    // to perform HTTP requests against our application
+    let client = reqwest::Client::new();
+
+    //Act
+    let response = client
+        .get(&format!("{}/health_check", &address))
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    // Assert;
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
+
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    let port = listener.local_addr().unwrap().port();
+    let server = zero_to_production::run(listener).expect("Failed to bind address");
+    let _ = tokio::spawn(server);
+
+    format!("http://127.0.0.1:{}", port)
+}
